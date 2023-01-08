329f95213aa2f8f776508d24329d95109148b519
Castillo <gpro1pro@gmail.com>
2023-01-08 04:51:03 -0500

    handle minus a plus created in cart

--- src/components/Cart/CartProduct.tsx
+++ src/components/Cart/CartProduct.tsx
@@ -11,8 +11,10 @@ interface CardProductProps {
   product: Cart;
 }
 const CartProduct = ({ product }: CardProductProps) => {
+  const [isLoading, setIsLoading] = useState(false);
   const dispatch: AppDispatch = useDispatch();
   const [imgProduct, setImgProduct] = useState("");
+  const { cart } = useSelector((state: RootState) => state);
 
   const { products } = useSelector((state: RootState) => state);
 
@@ -32,8 +34,26 @@ const CartProduct = ({ product }: CardProductProps) => {
       })
       .catch((err) => console.log(err));
   };
-  console.log(product);
-
+  const handleClick = (value: number) => {
+    setIsLoading(true);
+    const URLPatch = "https://e-commerce-api.academlo.tech/api/v1/cart";
+    const prevQuantity = cart.filter((e) => e.id === product.id)[0]
+      .productsInCart.quantity;
+    const data = {
+      id: product.id,
+      newQuantity: prevQuantity + value,
+    };
+    axios
+      .patch(URLPatch, data, getConfig())
+      .then((res) => {
+        dispatch(getUserCart());
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
+  };
   return (
     <tr>
       <td className="table__td">
@@ -46,10 +66,26 @@ const CartProduct = ({ product }: CardProductProps) => {
         <p className="table__name">{product.price}</p>
       </td>
       <td>
-        <p className="table__name">{product.productsInCart.quantity}</p>
+        {!isLoading?<p className="table__name table__name--quantity ">
+          {product.productsInCart.quantity == 1 ? (
+            <i className="fa-solid fa-less-than gray"></i>
+          ) : (
+            <i
+              className="fa-solid fa-less-than "
+              onClick={() => handleClick(-1)}
+            ></i>
+          )}
+          <span className="table__span">{product.productsInCart.quantity}</span>{" "}
+          <i
+            className="fa-solid fa-greater-than"
+            onClick={() => handleClick(+1)}
+          ></i>
+        </p>:<span className="loader"></span>}
       </td>
       <td>
-        <p className="table__name">{(product.productsInCart.quantity *  +product.price).toFixed(2) }</p>
+        <p className="table__name">
+          {(product.productsInCart.quantity * +product.price).toFixed(2)}
+        </p>
       </td>
       <td onClick={handleDelete}>
         <i className="fa-regular fa-trash-can"></i>
